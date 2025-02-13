d3a6992e1eaf015ed6a6c5fd853f05fef1e85fa5
hgherzog <henryh@allenai.org>
2025-02-12 21:27:28 -0800

    remove double constants

--- helios/constants.py
+++ helios/constants.py
@@ -1,31 +1,3 @@
 """Constants shared across the helios package."""
 
-S2_BANDS = [
-    "B01",
-    "B02",
-    "B03",
-    "B04",
-    "B05",
-    "B06",
-    "B07",
-    "B08",
-    "B8A",
-    "B09",
-    "B10",
-    "B11",
-    "B12",
-]
-
-LATLON_BANDS = ["lat", "lon"]
-TIMESTAMPS = ["day", "month", "year"]
-
-WORLDCOVER_BANDS = ["B1"]
-
-MODALITY_NAME_TO_BANDS = {
-    "sentinel2": S2_BANDS,
-    "latlon": LATLON_BANDS,
-    "timestamps": TIMESTAMPS,
-}
-
-
 BASE_GSD = 10  # What unit is this in?
