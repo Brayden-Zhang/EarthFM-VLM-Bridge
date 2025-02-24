6766cccc6acac39d270d5221f7c9580182ce9274
hgherzog <henryh@allenai.org>
2025-02-24 09:02:24 -0800

    wip writing tests

---
+++ tests/unit/data/test_visualize.py
@@ -0,0 +1,15 @@
+"""Unit tests for the Helios Dataset Visualization."""
+
+import pytest
+
+from helios.data.dataset import HeliosDataset
+from helios.data.visualize import visualize_sample
+
+
+
+
+
+def test_visualize_sample():
+    """Test the visualize_sample function."""
+    dataset = HeliosDataset()
+    visualize_sample(dataset, 0)
