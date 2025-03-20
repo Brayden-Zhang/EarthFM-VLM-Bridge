3eb13599994c8f84d9215cbf217790a64da41707
hgherzog <henryh@allenai.org>
2025-03-19 19:35:13 -0700

    updating settings

--- scripts/base_debug_scripts/galileo.py
+++ scripts/base_debug_scripts/galileo.py
@@ -128,7 +128,7 @@ def build_train_module_config(
     }
     token_exit_cfg_b = {modality: 0 for modality in common.supported_modality_names}
 
-    WARMUP_EPOCHS = 2
+    WARMUP_EPOCHS = 10
     dp_config = DataParallelConfig(name=DataParallelType.ddp)
 
     # TODO: would need a scheduler config and registry to be able to change this with overrides
