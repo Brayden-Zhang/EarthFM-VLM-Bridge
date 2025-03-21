24ad532794f50385b671a852d8a1fc69bb024256
Yawen Zhang <yawenz@allenai.org>
2025-03-21 15:10:45 -0700

    change to new loss

--- scripts/base_debug_scripts/latent_mim.py
+++ scripts/base_debug_scripts/latent_mim.py
@@ -100,7 +100,7 @@ def build_train_module_config(
     )
     loss_config = LossConfig(
         loss_config={
-            "type": "patch_discrimination",  # TODO: Should be registered via enum names
+            "type": "patch_discrimination_new",  # TODO: Should be registered via enum names
         }
     )
     token_exit_cfg = {modality: 0 for modality in common.supported_modality_names}
