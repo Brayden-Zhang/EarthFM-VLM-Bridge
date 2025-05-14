fa301d640d708cffb01c76ac0c567d9d514d124a
Joseph Redmon <pjreddie@gmail.com>
2025-05-14 13:27:10 -0700

    update ratio little

--- scripts/2025_05_13_mae/galileo_contrastive_mae.py
+++ scripts/2025_05_13_mae/galileo_contrastive_mae.py
@@ -103,8 +103,8 @@ def build_train_module_config(
     """Build the train module config for an experiment."""
     LR = 0.0001
     RANK_MICROBATCH_SIZE = 64
-    ENCODE_RATIO = 0.1
-    DECODE_RATIO = 0.9
+    ENCODE_RATIO = 0.2
+    DECODE_RATIO = 0.8
     WD = 0.02
     optim_config = AdamWConfig(lr=LR, weight_decay=WD)
     masking_config_a = MaskingConfig(
