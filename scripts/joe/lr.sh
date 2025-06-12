f8d74b8ce3d5bcd00a03e7c639b1f3cb9b19b5e6
Joseph Redmon <pjreddie@gmail.com>
2025-06-12 09:43:15 -0700

    higher lr poly

--- scripts/joe/lr.sh
+++ scripts/joe/lr.sh
@@ -7,4 +7,5 @@
 #python scripts/joe/latent_mim.py launch latent_mim_lr.001 ai2/titan-cirrascale --launch.priority=low --common.launch.num_gpus=1 --train_module.optim_config.lr=0.001
 #python scripts/joe/latent_mim_large.py launch latent_mim_large.001_ ai2/jupiter-cirrascale-2 --launch.priority=urgent --common.launch.num_gpus=8 --train_module.optim_config.lr=0.001
 #python scripts/joe/latent_mim_large.py launch latent_mim_large.0004_ ai2/jupiter-cirrascale-2 --launch.priority=urgent --common.launch.num_gpus=8 --train_module.optim_config.lr=0.0004
-python scripts/joe/latent_mim.py launch latent_mim_lr_poly ai2/jupiter-cirrascale-2 --launch.priority=urgent --common.launch.num_gpus=2
+#python scripts/joe/latent_mim.py launch latent_mim_lr_poly ai2/jupiter-cirrascale-2 --launch.priority=urgent --common.launch.num_gpus=2
+python scripts/joe/latent_mim.py launch latent_mim_lr0.001_poly ai2/jupiter-cirrascale-2 --launch.priority=urgent --common.launch.num_gpus=2 --train_module.optim_config.lr=0.001
