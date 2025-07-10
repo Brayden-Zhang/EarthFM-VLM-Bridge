421007926da035e11ee6f8cc637489eb121353cc
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2025-07-10 15:10:18 -0700

    run breizhcrops

--- helios/train/callbacks/evaluator_callback.py
+++ helios/train/callbacks/evaluator_callback.py
@@ -282,9 +282,9 @@ class DownstreamEvaluatorCallbackConfig(CallbackConfig):
 
             # Check that input_modalities is only set for multimodal tasks
             if (
-                task.dataset not in ["pastis", "sickle"]
-                and len(task.input_modalities) > 0
-            ):
+                (task.dataset not in ["pastis", "sickle"])
+                or not (task.dataset.startswith("cropharvest"))
+            ) and len(task.input_modalities) > 0:
                 raise ValueError(
                     f"input_modalities must be set for multimodal tasks, got {task.dataset}"
                 )
