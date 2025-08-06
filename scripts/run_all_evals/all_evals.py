595acf3249cf1fef0155bbc43bca2a748c423f8e
hgherzog <henryh@allenai.org>
2025-08-06 14:14:16 -0700

    include update d changes to make this work generally

--- scripts/run_all_evals/all_evals.py
+++ scripts/run_all_evals/all_evals.py
@@ -1,8 +1,9 @@
 """Launch script for evaluation allowing you to easily run all the evals for your model by just pointing at your training script."""
 
-import argparse
 import importlib.util
+import os
 import sys
+from logging import getLogger
 
 from olmo_core.train.callbacks import (
     BeakerCallback,
@@ -30,14 +31,211 @@ from helios.train.callbacks import (
 )
 from helios.train.callbacks.evaluator_callback import DownstreamTaskConfig
 
+logger = getLogger(__name__)
+
 
 def load_user_module(path):
     """Load the user module from the given path."""
+    logger.info(f"Loading user module from {path}")
     spec = importlib.util.spec_from_file_location("user_module", path)
     user_mod = importlib.util.module_from_spec(spec)
     sys.modules["user_module"] = user_mod
     spec.loader.exec_module(user_mod)
     return user_mod
+
+
+EVAL_TASKS = {
+    "m_forestnet": DownstreamTaskConfig(
+        dataset="m-forestnet",
+        embedding_batch_size=128,
+        num_workers=4,
+        pooling_type=PoolingType.MEAN,
+        norm_stats_from_pretrained=False,
+        eval_interval=Duration.epochs(5),
+    ),
+    "m_eurosat": DownstreamTaskConfig(
+        dataset="m-eurosat",
+        embedding_batch_size=128,
+        num_workers=0,
+        pooling_type=PoolingType.MEAN,
+        norm_stats_from_pretrained=False,  # True, #False,
+        eval_interval=Duration.epochs(5),
+    ),
+    "m_bigearthnet": DownstreamTaskConfig(
+        dataset="m-bigearthnet",
+        embedding_batch_size=64,
+        num_workers=4,
+        pooling_type=PoolingType.MEAN,
+        norm_stats_from_pretrained=False,
+        eval_interval=Duration.epochs(5),
+    ),
+    "m_so2sat": DownstreamTaskConfig(
+        dataset="m-so2sat",
+        embedding_batch_size=128,
+        num_workers=4,
+        pooling_type=PoolingType.MEAN,
+        norm_stats_from_pretrained=False,
+        eval_interval=Duration.epochs(5),
+    ),
+    "m_brick_kiln": DownstreamTaskConfig(
+        dataset="m-brick-kiln",
+        embedding_batch_size=128,
+        num_workers=4,
+        pooling_type=PoolingType.MEAN,
+        norm_stats_from_pretrained=False,  # True,
+        eval_interval=Duration.epochs(5),
+    ),
+    "mados": DownstreamTaskConfig(
+        dataset="mados",
+        embedding_batch_size=128,
+        probe_batch_size=128,
+        num_workers=8,
+        pooling_type=PoolingType.MEAN,
+        norm_stats_from_pretrained=False,
+        probe_lr=0.1,
+        eval_interval=Duration.epochs(10),
+    ),
+    "pastis_sentinel2": DownstreamTaskConfig(
+        dataset="pastis",
+        embedding_batch_size=32,
+        probe_batch_size=8,
+        num_workers=2,
+        pooling_type=PoolingType.MAX,
+        norm_stats_from_pretrained=False,
+        probe_lr=0.1,
+        eval_interval=Duration.epochs(50),
+        input_modalities=[Modality.SENTINEL2_L2A.name],
+        epochs=50,
+    ),
+    "breizhcrops": DownstreamTaskConfig(
+        dataset="breizhcrops",
+        embedding_batch_size=128,
+        probe_batch_size=128,
+        num_workers=0,
+        pooling_type=PoolingType.MAX,
+        norm_stats_from_pretrained=False,
+        eval_interval=Duration.epochs(50),
+        patch_size=1,
+        eval_mode="linear_probe",
+        probe_lr=0.1,
+        epochs=50,
+    ),
+    "sickle_landsat": DownstreamTaskConfig(
+        dataset="sickle",
+        embedding_batch_size=32,
+        probe_batch_size=16,
+        num_workers=2,
+        pooling_type=PoolingType.MEAN,
+        norm_stats_from_pretrained=False,
+        probe_lr=0.01,
+        eval_interval=Duration.epochs(10),
+        input_modalities=[Modality.LANDSAT.name],
+        epochs=50,
+    ),
+    "m_sa_crop_type": DownstreamTaskConfig(
+        dataset="m-sa-crop-type",
+        embedding_batch_size=32,
+        probe_batch_size=8,
+        num_workers=2,
+        pooling_type=PoolingType.MEAN,
+        norm_stats_from_pretrained=False,
+        probe_lr=0.1,
+        eval_interval=Duration.epochs(10),
+    ),
+    "m_cashew_plant": DownstreamTaskConfig(
+        dataset="m-cashew-plant",
+        embedding_batch_size=32,
+        probe_batch_size=8,
+        num_workers=2,
+        pooling_type=PoolingType.MEAN,
+        norm_stats_from_pretrained=False,
+        probe_lr=0.1,
+        eval_interval=Duration.epochs(10),
+    ),
+    "cropharvest_Togo_12_sentinel2": DownstreamTaskConfig(
+        dataset="cropharvest_Togo_12",
+        embedding_batch_size=128,
+        num_workers=2,
+        pooling_type=PoolingType.MEAN,
+        norm_stats_from_pretrained=True,
+        eval_interval=Duration.epochs(20),
+        input_modalities=[Modality.SENTINEL2_L2A.name],
+        patch_size=1,
+        eval_mode="linear_probe",
+        probe_lr=0.1,
+        epochs=50,
+    ),
+    "cropharvest_Togo_12_sentinel1": DownstreamTaskConfig(
+        dataset="cropharvest_Togo_12",
+        embedding_batch_size=128,
+        num_workers=2,
+        pooling_type=PoolingType.MEAN,
+        norm_stats_from_pretrained=True,
+        eval_interval=Duration.epochs(20),
+        input_modalities=[Modality.SENTINEL1.name],
+        patch_size=1,
+        eval_mode="linear_probe",
+        probe_lr=0.1,
+        epochs=50,
+    ),
+    # example of "in season" cropland mapping - 6 indicates only the
+    # first 6 timesteps are passed to the model
+    "cropharvest_Peoples_Republic_of_China_6": DownstreamTaskConfig(
+        dataset="cropharvest_People's Republic of China_6",
+        embedding_batch_size=128,
+        num_workers=2,
+        pooling_type=PoolingType.MEAN,
+        norm_stats_from_pretrained=True,
+        eval_interval=Duration.epochs(20),
+        input_modalities=[Modality.SENTINEL2_L2A.name],
+        patch_size=1,
+        eval_mode="linear_probe",
+        probe_lr=0.1,
+        epochs=50,
+    ),
+    "cropharvest_Peoples_Republic_of_China_6_sentinel1": DownstreamTaskConfig(
+        dataset="cropharvest_People's Republic of China_6",
+        embedding_batch_size=128,
+        num_workers=2,
+        pooling_type=PoolingType.MEAN,
+        norm_stats_from_pretrained=True,
+        eval_interval=Duration.epochs(20),
+        input_modalities=[Modality.SENTINEL1.name],
+        patch_size=1,
+        eval_mode="linear_probe",
+        probe_lr=0.1,
+        epochs=50,
+    ),
+    "cropharvest_Togo_12_sentinel2_sentinel1": DownstreamTaskConfig(
+        dataset="cropharvest_Togo_12",
+        embedding_batch_size=128,
+        num_workers=2,
+        pooling_type=PoolingType.MEAN,
+        norm_stats_from_pretrained=True,
+        eval_interval=Duration.epochs(20),
+        input_modalities=[Modality.SENTINEL2_L2A.name, Modality.SENTINEL1.name],
+        patch_size=1,
+        eval_mode="linear_probe",
+        probe_lr=0.1,
+        epochs=50,
+    ),
+    "cropharvest_Peoples_Republic_of_China_6_sentinel2_sentinel1_sentinel2": DownstreamTaskConfig(
+        dataset="cropharvest_People's Republic of China_6",
+        embedding_batch_size=128,
+        num_workers=2,
+        pooling_type=PoolingType.MEAN,
+        norm_stats_from_pretrained=True,
+        eval_interval=Duration.epochs(20),
+        input_modalities=[
+            Modality.SENTINEL2_L2A.name,
+            Modality.SENTINEL1.name,
+        ],
+        patch_size=1,
+        eval_mode="linear_probe",
+        probe_lr=0.1,
+        epochs=50,
+    ),
+}
 
 
 def build_trainer_config(common: CommonComponents) -> TrainerConfig:
@@ -59,115 +257,6 @@ def build_trainer_config(common: CommonComponents) -> TrainerConfig:
     )
     # Safe to collect everys tep for now
     garbage_collector_callback = GarbageCollectorCallback(gc_interval=1)
-    EVAL_TASKS = {
-        "m_forestnet": DownstreamTaskConfig(
-            dataset="m-forestnet",
-            embedding_batch_size=128,
-            num_workers=4,
-            pooling_type=PoolingType.MEAN,
-            norm_stats_from_pretrained=False,
-            eval_interval=Duration.epochs(5),
-        ),
-        "m_eurosat": DownstreamTaskConfig(
-            dataset="m-eurosat",
-            embedding_batch_size=128,
-            num_workers=0,
-            pooling_type=PoolingType.MEAN,
-            norm_stats_from_pretrained=False,  # True, #False,
-            eval_interval=Duration.epochs(5),
-        ),
-        "m_bigearthnet": DownstreamTaskConfig(
-            dataset="m-bigearthnet",
-            embedding_batch_size=64,
-            num_workers=4,
-            pooling_type=PoolingType.MEAN,
-            norm_stats_from_pretrained=False,
-            eval_interval=Duration.epochs(5),
-        ),
-        "m_so2sat": DownstreamTaskConfig(
-            dataset="m-so2sat",
-            embedding_batch_size=128,
-            num_workers=4,
-            pooling_type=PoolingType.MEAN,
-            norm_stats_from_pretrained=False,
-            eval_interval=Duration.epochs(5),
-        ),
-        "m_brick_kiln": DownstreamTaskConfig(
-            dataset="m-brick-kiln",
-            embedding_batch_size=128,
-            num_workers=4,
-            pooling_type=PoolingType.MEAN,
-            norm_stats_from_pretrained=False,  # True,
-            eval_interval=Duration.epochs(5),
-        ),
-        "mados": DownstreamTaskConfig(
-            dataset="mados",
-            embedding_batch_size=128,
-            probe_batch_size=128,
-            num_workers=8,
-            pooling_type=PoolingType.MEAN,
-            norm_stats_from_pretrained=False,
-            probe_lr=0.1,
-            eval_interval=Duration.epochs(10),
-        ),
-        "pastis_sentinel2": DownstreamTaskConfig(
-            dataset="pastis",
-            embedding_batch_size=32,
-            probe_batch_size=8,
-            num_workers=2,
-            pooling_type=PoolingType.MAX,
-            norm_stats_from_pretrained=False,
-            probe_lr=0.1,
-            eval_interval=Duration.epochs(50),
-            input_modalities=[Modality.SENTINEL2_L2A.name],
-            epochs=50,
-        ),
-        "breizhcrops": DownstreamTaskConfig(
-            dataset="breizhcrops",
-            embedding_batch_size=128,
-            probe_batch_size=128,
-            num_workers=0,
-            pooling_type=PoolingType.MAX,
-            norm_stats_from_pretrained=False,
-            eval_interval=Duration.epochs(50),
-            patch_size=1,
-            eval_mode="linear_probe",
-            probe_lr=0.1,
-            epochs=50,
-        ),
-        "sickle_landsat": DownstreamTaskConfig(
-            dataset="sickle",
-            embedding_batch_size=32,
-            probe_batch_size=16,
-            num_workers=2,
-            pooling_type=PoolingType.MEAN,
-            norm_stats_from_pretrained=False,
-            probe_lr=0.01,
-            eval_interval=Duration.epochs(10),
-            input_modalities=[Modality.LANDSAT.name],
-            epochs=50,
-        ),
-        "m_sa_crop_type": DownstreamTaskConfig(
-            dataset="m-sa-crop-type",
-            embedding_batch_size=32,
-            probe_batch_size=8,
-            num_workers=2,
-            pooling_type=PoolingType.MEAN,
-            norm_stats_from_pretrained=False,
-            probe_lr=0.1,
-            eval_interval=Duration.epochs(10),
-        ),
-        "m_cashew_plant": DownstreamTaskConfig(
-            dataset="m-cashew-plant",
-            embedding_batch_size=32,
-            probe_batch_size=8,
-            num_workers=2,
-            pooling_type=PoolingType.MEAN,
-            norm_stats_from_pretrained=False,
-            probe_lr=0.1,
-            eval_interval=Duration.epochs(10),
-        ),
-    }
     trainer_config = (
         TrainerConfig(
             work_dir=common.save_folder,
@@ -204,17 +293,10 @@ def build_trainer_config(common: CommonComponents) -> TrainerConfig:
 
 
 if __name__ == "__main__":
-    pre = argparse.ArgumentParser(add_help=False)
-    pre.add_argument(
-        "--module-path",
-        "-m",
-        type=str,
-        required=True,
-        help="path to .py that defines the same builders as dino_v2",
-    )
-    # 2) Grab that one, leave everything else untouched in `rest`
-    args, rest = pre.parse_known_args()
-    user_mod = load_user_module(args.module_path)
+    module_path = os.environ.get("TRAIN_SCRIPT_PATH")
+    if module_path is None:
+        raise ValueError("TRAIN_SCRIPT_PATH environment variable must be set")
+    user_mod = load_user_module(module_path)
 
     # 3) Inject all of the builder names into your namespace
     build_common_components = user_mod.build_common_components
--- scripts/run_all_evals/full_eval_sweep.py
+++ scripts/run_all_evals/full_eval_sweep.py
@@ -1,46 +1,46 @@
 """Run an evaluation sweep for DINOv2."""
 
 import argparse
+import os
 import subprocess  # nosec
 
+from all_evals import EVAL_TASKS
+
+from helios.evals.datasets.configs import dataset_to_config, get_eval_mode
+
 LP_LRs = [1e-4, 5e-4, 1e-3, 5e-3, 1e-2, 5e-2, 1e-1, 5e-1]
-Normalization_MODES = ["imagenet", "dataset", "helios"]
+Normalization_MODES = ["dataset", "helios"]
+
+
+def create_linear_probe_arg(task_name: str) -> str:
+    """Create a linear probe argument for a given task name."""
+    initial_str = (
+        f"--trainer.callbacks.downstream_evaluator.tasks.{task_name}.probe_lr="
+    )
+    return initial_str + "{lr}"
+
 
 lr_args = " ".join(
     [
-        "--trainer.callbacks.downstream_evaluator.tasks.m_cashew-plant.probe_lr={lr}",
-        "--trainer.callbacks.downstream_evaluator.tasks.mados.probe_lr={lr}",
-        "--trainer.callbacks.downstream_evaluator.tasks.pastis_sentinel2.probe_lr={lr}",
-        "--trainer.callbacks.downstream_evaluator.tasks.sickle_landsat.probe_lr={lr}",
-        "--trainer.callbacks.downstream_evaluator.tasks.breizhcrops.probe_lr={lr}",
+        create_linear_probe_arg(task_name)
+        for task_name, task in EVAL_TASKS.items()
+        if get_eval_mode(dataset_to_config(task.dataset).task_type) == "linear_probe"
     ]
 )
 
 dataset_args = " ".join(
-    [
-        " ",
-        "--trainer.callbacks.downstream_evaluator.tasks.m_eurosat.norm_stats_from_pretrained=False",
-        "--trainer.callbacks.downstream_evaluator.tasks.m_forestnet.norm_stats_from_pretrained=False",
-        "--trainer.callbacks.downstream_evaluator.tasks.m_bigearthnet.norm_stats_from_pretrained=False",
-        "--trainer.callbacks.downstream_evaluator.tasks.m_so2sat.norm_stats_from_pretrained=False",
-        "--trainer.callbacks.downstream_evaluator.tasks.m_brick_kiln.norm_stats_from_pretrained=False",
-        "--trainer.callbacks.downstream_evaluator.tasks.m_cashew-plant.norm_stats_from_pretrained=False",
-        "--trainer.callbacks.downstream_evaluator.tasks.mados.norm_stats_from_pretrained=False",
-        "--trainer.callbacks.downstream_evaluator.tasks.pastis_sentinel2.norm_stats_from_pretrained=False",
+    [" "]
+    + [
+        f"--trainer.callbacks.downstream_evaluator.tasks.{task_name}.norm_stats_from_pretrained=False"
+        for task_name in EVAL_TASKS.keys()
     ]
 )
 
 helios_args = " ".join(
-    [
-        " ",
-        "--trainer.callbacks.downstream_evaluator.tasks.m_eurosat.norm_stats_from_pretrained=True",
-        "--trainer.callbacks.downstream_evaluator.tasks.m_forestnet.norm_stats_from_pretrained=True",
-        "--trainer.callbacks.downstream_evaluator.tasks.m_bigearthnet.norm_stats_from_pretrained=True",
-        "--trainer.callbacks.downstream_evaluator.tasks.m_so2sat.norm_stats_from_pretrained=True",
-        "--trainer.callbacks.downstream_evaluator.tasks.m_brick_kiln.norm_stats_from_pretrained=True",
-        "--trainer.callbacks.downstream_evaluator.tasks.m_cashew-plant.norm_stats_from_pretrained=True",
-        "--trainer.callbacks.downstream_evaluator.tasks.mados.norm_stats_from_pretrained=True",
-        "--trainer.callbacks.downstream_evaluator.tasks.pastis_sentinel2.norm_stats_from_pretrained=True",
+    [""]
+    + [
+        f"--trainer.callbacks.downstream_evaluator.tasks.{task_name}.norm_stats_from_pretrained=True"
+        for task_name in EVAL_TASKS.keys()
     ]
 )
 
@@ -50,10 +50,10 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--cluster", type=str, required=True, help="Cluster name")
     parser.add_argument(
-        "--checkpoint-path", type=str, required=True, help="Checkpoint path"
+        "--checkpoint_path", type=str, required=True, help="Checkpoint path"
     )
     parser.add_argument(
-        "--module-path", type=str, required=True, help="Path to module .py"
+        "--module_path", type=str, required=True, help="Path to module .py"
     )
     args = parser.parse_args()
 
@@ -66,14 +66,17 @@ def main():
     for lr in LP_LRs:
         for norm_mode in Normalization_MODES:
             print(f"Running with {norm_mode} normalization and {lr} learning rate")
-            run_name = f"eval_{norm_mode}_lr{lr}"
+
+            parent_dir = os.path.basename(os.path.dirname(checkpoint_path))
+            base_run_name = f"test_{parent_dir}_{norm_mode}_lr{lr}"
+            run_name = base_run_name[:100]
             cmd_args = lr_args.format(lr=lr)
             if norm_mode == "dataset":
                 cmd_args += dataset_args
             elif norm_mode == "helios":
                 cmd_args += helios_args
             cmd = (
-                f"python3 scripts/run_all_evals/all_evals.py -m {module_path} "
+                f"TRAIN_SCRIPT_PATH={module_path} python3 scripts/run_all_evals/all_evals.py "
                 f"launch {run_name} {cluster} --launch.priority=high {cmd_args} "
                 f"--launch.task_name=eval --trainer.load_path={checkpoint_path}"
             )
