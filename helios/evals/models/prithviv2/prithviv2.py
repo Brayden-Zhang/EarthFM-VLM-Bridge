eb94b819dfa994d86dddcbe9343e9ce1a7db8263
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2025-10-07 17:35:23 +0100

    Update prithvi repository

--- helios/evals/models/prithviv2/prithviv2.py
+++ helios/evals/models/prithviv2/prithviv2.py
@@ -40,19 +40,19 @@ PRITHVI_STD = [
 class PrithviV2Models(StrEnum):
     """Names for different Prithvi models on torch hub."""
 
-    VIT_300 = "Prithvi_EO_V2_300M"
-    VIT_600 = "Prithvi_EO_V2_600M"
+    VIT_300 = "Prithvi-EO-2.0-300M"
+    VIT_600 = "Prithvi-EO-2.0-600M"
 
 
 MODEL_TO_HF_INFO = {
     PrithviV2Models.VIT_300: {
         "hf_hub_id": f"ibm-nasa-geospatial/{PrithviV2Models.VIT_300.value}",
-        "weights": f"{PrithviV2Models.VIT_300.value}.pt",
+        "weights": "Prithvi_EO_V2_300M.pt",
         "revision": "b2f2520ab889f42a25c5361ba18761fcb4ea44ad",
     },
     PrithviV2Models.VIT_600: {
         "hf_hub_id": f"ibm-nasa-geospatial/{PrithviV2Models.VIT_600.value}",
-        "weights": f"{PrithviV2Models.VIT_600.value}.pt",
+        "weights": "Prithvi_EO_V2_600M.pt",
         "revision": "87f15784813828dc37aa3197a143cd4689e4d080",
     },
 }
